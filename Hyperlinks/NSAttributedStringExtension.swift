323d8ffd30e44efdf4a2a4a0e80fb4f16faaefe9
Manoj Daswani <ManojDaswani11@gmail.com>
2020-11-03 13:29:32 +0530

    NSAttributedStringExtension.swift, ViewController, SceneDelegate & App Delegate 3rd Nov 2020

---
+++ Hyperlinks/NSAttributedStringExtension.swift
@@ -0,0 +1,37 @@
+//
+//  NSAttributedStringExtension.swift
+//  Hyperlinks
+//
+//  Created by Manoj Daswani on 3/11/20.
+//
+
+import Foundation
+
+/* The reason why we are creating an NSAttributedString in order to make the text hyperlink and we need to have that accessible anywhere we create NSAttributed.
+ We also wantwed to a static string because, we could say NSAttributedString. or function name that should return correctly attributed formated string
+ */
+
+extension NSAttributedString {
+    // for path - location of the text  or URL
+    // in string: referring to the Text area
+    // as substring: referring to what is tappable "Term" and "Policy"
+    
+    
+    static func makeHyperlink(for path: String, in string: String, as substring: String) -> NSAttributedString{
+        
+        // to get the "range" of the "substring" inside of the NSString
+        // we want to ensure we get NSRange back, not a regular range
+        
+        let nsString = NSString(string: string)
+        let substringRange = nsString.range(of: substring)
+        
+        // we want to create the attributedString and then
+        // modify it to hyperlink the substring that is in substringRange
+        
+        let attributedString = NSMutableAttributedString(string: string)
+        attributedString.addAttribute(.link, value: path, range: substringRange)
+        return attributedString
+        
+    }
+}
+
--- Hyperlinks/ViewController.swift
+++ Hyperlinks/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateTextView()
     }
-
-
+    
+    func updateTextView() {
+        
+        // the values below are from extension NSAttributedString
+        let path = "https://wordpress.com/tos"
+        let text = textView.text ?? ""
+        let attributedString = NSAttributedString.makeHyperlink(for: path, in: text, as: "Terms of Service")
+        textView.attributedText = attributedString
+    }
+    
 }
 
